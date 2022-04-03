71ef440644d55e8b2b3ab56afae239058eea36d7
Steven Dwy <yoplitein+git@gmail.com>
2022-04-03 03:02:59 -0500

    Implement background task, add help messages

--- janitor/__init__.py
+++ janitor/__init__.py
@@ -1,12 +1,18 @@
+import asyncio
 import datetime
 import logging
 import os
-from typing import Optional
+from typing import List, Optional
 
 import discord
 from discord.ext import commands, tasks
 
 from .db import isSweepEnabled, addChannel, removeChannel, getAllChannels, setMaxAge, getMaxAge
+
+EMOJI_CHECK = "\u2705"
+EMOJI_CROSS = "\u274E"
+EMOJI_STOP = "\u23F9"
+EMOJI_POOP = "\U0001F4A9"
 
 def maxAgeRepr(maxAge):
 	seconds = maxAge * 60
@@ -20,17 +26,23 @@ def maxAgeRepr(maxAge):
 	if minutes > 0 or len(ret) == 0: ret.append(f"{minutes} minutes")
 	return ", ".join(ret)
 
-def findChannel(guild: discord.Guild, channel: int):
-	match = [x for x in guild.channels if x.id == channel]
+def findChannel(guild: discord.Guild, channelID: int):
+	match = [x for x in guild.channels if x.id == channelID]
 	match len(match):
 		case 0:
-			logging.warning(f"Channel id {channel} does not exist on its guild ({guild!r})")
+			logging.warning(f"Channel id {channelID} does not exist on its guild ({guild!r})")
 		case 1:
 			return match[0]
 		case _:
-			logging.error(f"Channel id {channel} is duplicated {len(match)} times in guild {guild!r}")
+			logging.error(f"Channel id {channelID} is duplicated {len(match)} times in guild {guild!r}")
 
-async def reactionReply(ctx: commands.Context, emoji = "\u2705"):	
+def findChannels(guild: discord.Guild, channelIDs: List[int]):
+	return list(filter(
+		lambda x: x is not None,
+		(findChannel(guild, id) for id in channelIDs)
+	))
+
+async def reactionReply(ctx: commands.Context, emoji = EMOJI_CHECK):
 	await ctx.message.add_reaction(emoji)
 
 class Janitor(commands.Cog):
@@ -43,7 +55,19 @@ class Janitor(commands.Cog):
 	
 	@tasks.loop(minutes = 1)
 	async def sweepTask(self):
-		print("sweep task")
+		logging.debug("Running sweep task")
+		
+		for guild in self.bot.guilds:
+			channels = findChannels(guild, getAllChannels(guild.id))
+			logging.debug(f"Sweeping {len(channels)} channels in guild `{guild.name}`")
+			
+			for channel in channels:
+				logging.debug(f" => Sweeping `#{channel.name}`")
+				await self.sweepChannel(channel)
+	
+	@sweepTask.before_loop
+	async def beforeSweepTask(self):
+		await self.bot.wait_until_ready()
 	
 	async def sweepChannel(self, channel: discord.TextChannel, ignoreAge = False):
 		now = datetime.datetime.utcnow()
@@ -70,12 +94,17 @@ class Janitor(commands.Cog):
 		
 		await self.bot.change_presence(status = discord.Status.online, activity = None)
 	
+	async def cog_check(self, ctx: commands.Context):
+		if ctx.author.guild_permissions.administrator:
+			return True
+		raise commands.errors.MissingPermissions(["administrator"])
+	
 	@commands.group(help = "Manage list of channels that should be swept")
 	async def channels(self, ctx: commands.Context):
 		if ctx.invoked_subcommand is None:	
 			raise commands.errors.UserInputError("Expected a subcommand")
 	
-	@channels.command()
+	@channels.command(help = "Add a channel to be swept")
 	async def add(self, ctx: commands.Context, channel: discord.TextChannel):
 		if isSweepEnabled(channel.id):
 			await ctx.reply(f"I am already sweeping {channel.mention}")
@@ -83,7 +112,7 @@ class Janitor(commands.Cog):
 			addChannel(channel.id, ctx.guild.id)
 			await reactionReply(ctx)
 	
-	@channels.command()
+	@channels.command(help = "Remove a channel from sweep list")
 	async def remove(self, ctx: commands.Context, channel: discord.TextChannel):
 		if not isSweepEnabled(channel.id):
 			await ctx.reply(f"I am not currently sweeping {channel.mention}")
@@ -91,19 +120,16 @@ class Janitor(commands.Cog):
 			removeChannel(channel.id)
 			await reactionReply(ctx)
 	
-	@channels.command()
+	@channels.command(help = "Show all channels in this server being swept")
 	async def list(self, ctx: commands.Context):
-		channels = list(filter(
-			lambda x: x is not None,
-			(findChannel(ctx.guild, id) for id in getAllChannels(ctx.guild.id))
-		))
+		channels = findChannels(getAllChannels(ctx.guild.id))
 		if len(channels) > 0:
 			channelsStr = "\n\t".join(channel.mention for channel in channels)
 			await ctx.reply(f"""In `{ctx.guild.name}` I am configured to sweep:\n\t{channelsStr}""")
 		else:
 			await ctx.reply(f"I'm not configured to sweep any channels in `{ctx.guild.name}`")
 	
-	@commands.command()
+	@commands.command(help = "Get/set age (in minutes) of messages which should be swept, for current channel")
 	async def maxage(self, ctx: commands.Context, ageMinutes: Optional[int]):
 		if not isSweepEnabled(ctx.channel.id):
 			await ctx.reply(f"Sweeping is not enabled for {ctx.channel.mention}")
@@ -117,28 +143,71 @@ class Janitor(commands.Cog):
 			setMaxAge(ctx.channel.id, maxAge)
 		await ctx.reply(f"Messages in {ctx.channel.mention} will be deleted after {maxAgeRepr(maxAge)}")
 	
-	@commands.command()
+	@commands.command(help = "Sweep the current channel immediately, optionally ignoring message ages")
 	async def sweepnow(self, ctx: commands.Context, ignoreAge: Optional[bool] = False):
 		if not isSweepEnabled(ctx.channel.id):
 			await ctx.reply(f"Sweeping is not enabled for {ctx.channel.mention}")
 			return
+		
+		if ignoreAge:
+			confirmMsg = await ctx.reply(f"Are you sure you want to delete **all unpinned messages** in {ctx.channel.mention}, regardless of age?")
+			await confirmMsg.add_reaction(EMOJI_CHECK)
+			await confirmMsg.add_reaction(EMOJI_CROSS)
+			
+			try:
+				reaction, _ = await self.bot.wait_for(
+					"reaction_add",
+					timeout = 15,
+					check = lambda reaction, user:
+						user == ctx.message.author and str(reaction.emoji) in [EMOJI_CHECK, EMOJI_CROSS]
+				)
+			except asyncio.TimeoutError:
+				await reactionReply(ctx, EMOJI_POOP)
+				return
+			finally:
+				await confirmMsg.delete()
+			
+			if reaction.emoji.encode() != EMOJI_CHECK.encode():
+				await reactionReply(ctx, EMOJI_STOP)
+				return
 		
 		async with ctx.channel.typing():
 			await self.sweepChannel(ctx.channel, ignoreAge)
 			if not ignoreAge: # message will have been deleted
 				await reactionReply(ctx)
 
+async def onCommandError(ctx: commands.Context, error: commands.errors.CommandError):
+	logging.info(f"onCommandError handling uncaught exception", exc_info = error, stack_info = True)
+	await reactionReply(ctx, EMOJI_POOP) # oopsie poopsie
+	
+	if isinstance(error, commands.errors.CommandNotFound):
+		await ctx.reply(f"{error.args[0]}")
+		await ctx.send_help()
+	elif isinstance(error, commands.errors.UserInputError):
+		await ctx.reply(f"{error.args[0]}")
+		if ctx.command: await ctx.send_help(ctx.command)
+		elif ctx.cog: await ctx.send_help(ctx.cog)
+	elif isinstance(error, commands.errors.MissingPermissions):
+		await ctx.reply(f"{error.args[0]}")
+	elif isinstance(error, commands.errors.CommandInvokeError):
+		await ctx.reply("Oopsie poopsie! I had a stroke trying to process that")
+
 def makeBot():
 	intents = discord.Intents.default()
 	# intents.message_content = True # FIXME: this needs to be enabled in future discord.py versions
 	
-	bot = commands.Bot(command_prefix = commands.when_mentioned, intents = intents)
+	bot = commands.Bot(
+		command_prefix = commands.when_mentioned,
+		intents = intents,
+		description = "Deletes old messages from certain channels."
+	)
+	bot.on_command_error = onCommandError
 	bot.add_cog(Janitor(bot))
 	
 	return bot
 
-def main(bot = None):
-	logging.basicConfig(level = logging.INFO)
+def main(bot = None, logLevel = logging.WARNING):
+	logging.basicConfig(level = logLevel)
 	
 	bot = bot or makeBot()
 	
